cd1a32163bae4372f89ef341660b3f9e8efeefc6
Ali Doğan <100559467+midmotor@users.noreply.github.com>
2023-09-30 22:28:28 +0300

    Update keygen.go

--- keygen.go
+++ keygen.go
@@ -28,3 +28,22 @@ func (keyr2 keygenr2) Init(m int, keyr1 keygenr1, curvetype string) keygenr2 {
 
 	return keyr2
 }
+
+// just for one boss whose index is 1
+func (tasskeyr2 keygenr2) TassaInit(m int, keyr1 keygenr1, curvetype string) keygenr2 {
+	curve := getCurve(curvetype)
+	tasskeyr2.secrets = make([]curves.Scalar, m)
+	tasskeyr2.secrets[0] = keyr1.poly.Evaluate(curve.Scalar.New(1))
+
+	dpoly := derivative(keyr1.poly.Coefficients, curvetype)
+	temppoly := new(Polynomial).Init(keyr1.poly.threshold-1, curvetype)
+
+	for i := 0; i < len(dpoly); i++ {
+		temppoly.Coefficients[i] = dpoly[i]
+	}
+	for i := 1; i < m; i++ {
+		tasskeyr2.secrets[i] = temppoly.Evaluate(curve.Scalar.New(i + 1)).Clone()
+	}
+
+	return tasskeyr2
+}
