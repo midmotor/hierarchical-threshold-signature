ba451b30bbebd83e7318a8f8d1264ffd809f6480
Ali Doğan <100559467+midmotor@users.noreply.github.com>
2023-09-30 22:29:04 +0300

    Update basic.go

--- basic.go
+++ basic.go
@@ -2,6 +2,7 @@ package frost
 
 import (
 	"errors"
+	"math"
 
 	"github.com/coinbase/kryptology/pkg/core/curves"
 )
@@ -74,4 +75,68 @@ func secretcheck(id curves.Scalar, sskey curves.Scalar, com []curves.Point, t in
 	} else {
 		return errors.New("the secret value sent by the participant and the commitment values did not match.")
 	}
+}
+
+func derivative(coefficients []curves.Scalar, curvetype string) []curves.Scalar {
+	curve := getCurve(curvetype)
+	degree := len(coefficients) - 1
+	derivativeCoefficients := make([]curves.Scalar, degree)
+
+	for i := 1; i <= degree; i++ {
+		derivativeCoefficients[i-1] = coefficients[i].Mul(curve.Scalar.New(i))
+	}
+
+	return derivativeCoefficients
+}
+
+// for tassa commit
+func derivcommit(com []curves.Point, curvetype string) []curves.Point {
+	curve := getCurve(curvetype)
+	newthresh := len(com) - 1
+	newcom := make([]curves.Point, newthresh)
+
+	for i := 0; i < newthresh; i++ {
+		newcom[i] = com[i+1].Mul(curve.Scalar.New(i + 1))
+
+	}
+
+	return newcom
+
+}
+
+// 1 boss whose is index 1, other is emplo.
+func tassacoeff(t int, curvetype string) [][]curves.Scalar {
+	curve := getCurve(curvetype)
+	matrix := make([][]curves.Scalar, t)
+
+	for i := range matrix {
+		matrix[i] = make([]curves.Scalar, t)
+	}
+
+	firstrow := make([]curves.Scalar, t)
+
+	for i := 0; i < t; i++ {
+		firstrow[i] = curve.Scalar.New(1)
+
+	}
+	matrix[0] = firstrow
+
+	for i := 0; i < t; i++ {
+		firstrow[i] = curve.Scalar.New(1)
+
+	}
+
+	for i := 1; i < t; i++ {
+		for j := 0; j < t; j++ {
+			if j == 0 {
+				matrix[i][j] = curve.Scalar.Zero()
+			} else if j == 1 {
+				matrix[i][j] = curve.Scalar.New(1)
+			} else {
+				matrix[i][j] = curve.Scalar.New(j).Mul(curve.Scalar.New(int(math.Pow(float64(i+1), float64(j-1)))))
+			}
+		}
+	}
+
+	return matrix
 }
